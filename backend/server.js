b730d386896b7515d328c4de6f46c986d37f5ae9
Tetiana Khyzhniak <khyzhnjaktetjana@gmail.com>
2026-05-05 21:21:43 +0200

    add scripts for running project

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,11 @@ app.use(express.json());
 
 app.use("/laws", lawsRoutes);
 
-const PORT = 5000;
+app.get("/", (req, res) => {
+  res.send("API is running 🚀");
+});
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
